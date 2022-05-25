183ac7a8faf52936b4f77c83f63572cc2d545caf
Charles I <58774551+CGI3@users.noreply.github.com>
2022-05-25 15:46:31 -0400

    Update text_adventure_game.py
    Got rid of messy space

--- text_adventure_game.py
+++ text_adventure_game.py
@@ -27,8 +27,7 @@ print("Your mission is to find the treasure.")
 first_input = input('You\'re at a crossroad. Where do you want to go? Type "left" or "right"\n')
 
 if first_input == "left":
-  second_input = input('You\'ve come to a lake. There is an island in the middle   of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.\n')
-  
+  second_input = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.\n')
   if second_input == "wait":
     third_input = input('You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?\n')
     if third_input == "red":
